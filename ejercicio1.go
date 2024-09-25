0934ce9edc6398f4a1f4a58304b056d4c2c137cd
Aprendiz Tarde <Aprendiz Tarde@YPLAPRSSVFSW018.FormacionSena.RED>
2024-09-25 14:53:04 -0500

    creacion de ejercicios go

---
+++ ejercicio1.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func imprimir() {
+// 	fmt.Println("texto desde la funcion imprimir")
+// }
+
+// func hola_string(s string) string {
+// 	return "hola" + s
+// }
+// func sumar(a int, b int) int {
+// 	return a + b
+// }
+
+// func comparar_bool() {
+// 	b := true
+// 	a := false
+// 	if a == b {
+// 		fmt.Println(" a y b son iguales")
+// 	} else {
+// 		fmt.Println(" a y b no son iguales")
+// 	}
+// }
+// func arreglo() {
+// 	var aprendices [5]string
+
+// 	aprendices[0] = "leonardo"
+// 	aprendices[1] = " juan sebastian"
+// 	aprendices[2] = "frank"
+// 	aprendices[3] = "juan jose"
+// 	aprendices[4] = "jaider"
+// 	fmt.Println(aprendices)
+
+// func tipo_datos() {
+// 	var texto string = "fabian"
+// 	var numero int = 1000
+// 	var decimal float64 = 0.00001
+
+//	fmt.Println(reflect.TypeOf(texto))
+//	fmt.Println(reflect.TypeOf(numero))
+//
+// fmt.Println(reflect.TypeOf(decimal))
+
+// func coverstringtoboolean() {
+// 	var texto string = "true"
+
+// 	boolean, err := strconv.ParseBool(texto)
+// 	fmt.Println(boolean, reflect.TypeOf(boolean))
+// 	fmt.Println("este es el error: ", err)
+// }
+
+// func coverbooltostring() {
+// 	var palabra_bool bool = true
+// 	strbool := strconv.FormatBool(palabra_bool)
+// 	fmt.Println(strbool, reflect.TypeOf(strbool))
+// }
+
+func variables() {
+	// 	var (
+	// 		nombre, apellido string = "danna", "perez"
+	// 		edad             int    = 25
+	// 	)
+
+	// 	fmt.Println("el nombre es", nombre)
+	// 	fmt.Println("el apellido es", apellido)
+	// 	fmt.Println("la concatenacion de nombre y apellido es", nombre+" "+apellido)
+	// 	fmt.Println("la edad es", edad)
+
+
+// }
+
+// const pi = 3.1416
+
+// func area_circulo(radio float64) float64 {
+// 	return pi * (radio * radio)
+// }
+
+// func main() {
+// 	fmt.Println("el area del circulo es: ", area_circulo(3))
+
+
+	
+	//fmt.Println("texto desde la funcion main")
+	//imprimir()
+	//fmt.println(hola_string("nata"))
+	//fmt.Println(sumar(3,5))
+	//comparar_bool()
+
+	// arreglo()
+	// tipo_datos()
+	// coverstringtoboolean()
+
+	//coverbooltostring()
+	// variables()
+}
